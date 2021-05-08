e98e66c328b1a25575c400735e68d16ac6cd59ee
Mark Haskins <mark.haskins01@gmail.com>
2021-05-08 10:45:46 +0100

    .

---
+++ DomainSetup.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $DomainPass,
+    [Parameter()]
+    [String]
+    $DomainFQDN
+)
+$DomainNB = $DomainFQDN.Split(".")[0]
+$password = ConvertTo-SecureString $DomainPass -AsPlainText -Force
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "E:\windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName $DomainFQDN `
+-DomainNetbiosName $DomainNB `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "E:\windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "E:\windows\SYSVOL" `
+-Force:$true `
+-SafeModeAdministratorPassword: $password
