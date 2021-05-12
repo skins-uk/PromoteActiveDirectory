c855581f0b20db260d4b91f52f97dd0c7550b7e4
skins-uk <52573122+skins-uk@users.noreply.github.com>
2021-05-12 12:01:33 +0100

    Add files via upload

---
+++ LabStructure.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $SuppliedPass,
+    [Parameter()]
+    [String]
+    $UserNum    
+)
+#Setup Variables
+$DCRoot = ([ADSI]"").distinguishedName
+$DCUSERDNSDOMAIN = $env:USERDNSDOMAIN
+$WVDRoot = "OU=WVD,$($DCRoot)"
+$password = ConvertTo-SecureString "$($SuppliedPass)" -AsPlainText -Force
+#Create Root WVD OU
+New-ADOrganizationalUnit -Name "WVD" -Path $DCRoot -ProtectedFromAccidentalDeletion $False -Description "WVD Environment"
+#Create Other OUs
+New-ADOrganizationalUnit -Name "Users" -Path $WVDRoot -ProtectedFromAccidentalDeletion $False -Description "WVD Users"
+New-ADOrganizationalUnit -Name "Service Accounts" -Path $WVDRoot -ProtectedFromAccidentalDeletion $False -Description "WVD Service Accounts"
+New-ADOrganizationalUnit -Name "Servers" -Path $WVDRoot -ProtectedFromAccidentalDeletion $False -Description "WVD Servers"
+New-ADOrganizationalUnit -Name "Session Hosts" -Path $WVDRoot -ProtectedFromAccidentalDeletion $False -Description "WVD WVD Session Hosts"
+#Create Student Accounts within Domain
+foreach ($User in $UserNum) {
+    New-ADUser -Name "Student$($User) Account" -AccountPassword $password -DisplayName "Student$($User) Account" -Enabled $True -Path "OU=Users,$($WVDRoot)" -SamAccountName "Student$($User)" -Surname "Student$($User)" -UserPrincipalName "Student$($User)@$($DCUSERDNSDOMAIN)"    
+}
