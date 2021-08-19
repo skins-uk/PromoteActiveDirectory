efb73cc2806d1b71dd144e2336b866469e3e9e98
skins-uk <52573122+skins-uk@users.noreply.github.com>
2021-08-19 15:24:36 +0100

    .

--- baselab_DCSetup.ps1
+++ baselab_DCSetup.ps1
@@ -4,7 +4,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 #Install Az.DesktopVirtualization and configure Start Virtual Machine on resource group and host pool
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name Az.DesktopVirtualization -RequiredVersion 2.1.0 -force
+<# Install-Module -Name Az.DesktopVirtualization -RequiredVersion 2.1.0 -force
 Install-Module -Name Az -force
 #Choco install and Choco Apps
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
@@ -14,7 +14,7 @@ choco install notepadplusplus -y
 choco install winscp -y
 choco install sysinternals -y
 choco install bginfo -y
-#Setup and Partition Data Disk
+#> #Setup and Partition Data Disk
 Get-Disk | Where partitionstyle -eq 'raw' | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel 'Data' -Confirm:$false 
 #Allow Ping
 Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -enabled True
